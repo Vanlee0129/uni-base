3c8811e6c035633e20f7b1a2b07dd9ec3741357b
Vanlee0129 <33856668+Vanlee0129@users.noreply.github.com>
2020-10-10 11:04:27 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import App from './App'
+import uView from "uview-ui"
+import store from '@/store'
+const vuexStore = require("@/store/$u.mixin.js")
+
+Vue.mixin(vuexStore)
+
+Vue.use(uView)
+
+Vue.config.productionTip = false
+
+App.mpType = 'app'
+
+const app = new Vue({
+  store,
+  ...App
+})
+
+// http拦截器，此为需要加入的内容，如果不是写在common目录，请自行修改引入路径
+import httpInterceptor from '@/common/http.interceptor.js'
+// 这里需要写在最后，是为了等Vue创建对象完成，引入"app"对象(也即页面的"this"实例)
+Vue.use(httpInterceptor, app)
+
+app.$mount()
