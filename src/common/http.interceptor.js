e84b030f8688625d05258945546a2ab10da73d5a
Vanlee0129 <33856668+Vanlee0129@users.noreply.github.com>
2020-10-25 16:33:33 +0800

    Update http.interceptor.js

--- src/common/http.interceptor.js
+++ src/common/http.interceptor.js
@@ -5,7 +5,7 @@ const install = (Vue, vm) => {
         // 设置为json，返回后会对数据进行一次JSON.parse()
         dataType: 'json',
         showLoading: true, // 是否显示请求中的loading
-        loadingText: '请求中...', // 请求loading中的文字提示
+        loadingText: '正在加载...', // 请求loading中的文字提示
         loadingTime: 800, // 在此时间内，请求还没回来的话，就显示加载中动画，单位ms
         originalData: false, // 是否在拦截器中返回服务端的原始数据
         loadingMask: true, // 展示loading的时候，是否给一个透明的蒙层，防止触摸穿透
